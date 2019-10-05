3d36ca7227511c1bb6b1cfe90d34d71c590f8ef9
domworkhome <domworkhome@gmail.com>
2019-10-05 07:57:26 +0200

    Ajout des méthodes permettant de : - voir la fiche détaillée d'un voisin - mettre ou supprimer un voisin des favoris depuis sa fiche détaillée - accéder à la liste des favoris - ouvrir la fiche détaillée d'un voisin depuis la liste des favoris - mettre ou supprimer un voisin des favoris depuis sa fiche détaillée dans la liste des favoris
    Rédaction du fichier readme.md expliquant comment exécuter et compiler l'application.
    
    Mise en place des tests unitaires et instrumentalisés.

---
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/utils/SelectViewAction.java
@@ -0,0 +1,24 @@
+package com.openclassrooms.entrevoisins.utils;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.view.View;
+import com.openclassrooms.entrevoisins.R;
+import org.hamcrest.Matcher;
+
+public class SelectViewAction implements ViewAction {
+    @Override
+    public Matcher<View> getConstraints() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Click on specific button";
+    }
+
+    @Override
+    public void perform(UiController uiController, View view) {
+        view.performClick();
+    }
+}
